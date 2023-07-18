b24b80aa627ee4eb6c4b477d6c17f7a4b5d00540
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2023-07-18 12:42:34 +0530

    Create REST API

--- route.js
+++ route.js
@@ -1,17 +1,72 @@
 const express = require('express');
 const router = express.Router();
-
+const objectId = require('mongoose').Types.ObjectId;
 const Employee = require('./Employee');
 
-router.get('/', (req, res) => {
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+    if (objectId.isValid(id)) {
+        const employee = await Employee.findById(id);
+        if (employee) {
+            res.status(200).send(employee)
+        }
+        else {
+            res.status(400).send(`Record Not found with id: ${id}`);
+        }
+    }
+    else {
+        res.status(400).send(`${id} is Not Valid`);
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id
+    if (objectId.isValid(id)) {
+        const employee = await Employee.findByIdAndDelete(id);
+        res.status(200).send(employee)
+    }
+    else {
+        res.status(400).send(`Record Not found with id: ${id}`);
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    const id = req.params.id
+ 
+
+    if (objectId.isValid(id)) {
+        const employee = await Employee.findByIdAndUpdate(id, { name: req.body.name,
+            position: req.body.position,
+            dept: req.body.dept }, { new: true });
+        if (employee) {
+            res.status(200).send(employee)
+        }
+        else {
+            res.status(400).send(`Record Not found with id: ${id}`);
+        }
+    }
+    else {
+        res.status(400).send(`${id} is Not Valid`);
+    }
+})
+
+
+router.get('/', async (req, res) => {
+    const data = await Employee.find({})
+    res.send(data)
+})
+
+
+router.post('/', (req, res) => {
     let emp = new Employee({
         name: req.body.name,
         position: req.body.position,
         dept: req.body.dept
     })
 
-    emp.save().then(res.send(emp)).catch((err)=>{
-         console.log(err)
+    emp.save().then(res.send(emp)).catch((err) => {
+        console.log(err)
     })
 })
 
